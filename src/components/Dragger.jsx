8dcaaea7ba5cd13987de44def2837b4b6b402aa0
NikBK <nikhilbalakrishna5@gmail.com>
2024-06-02 23:22:40 +0530

    adds title to the dragger node

--- src/components/Dragger.jsx
+++ src/components/Dragger.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const Dragger = ({ dragStart, classes, content, icon }) => {
     return (
-        <div draggable onDragStart={dragStart} className={classes}>
+        <div draggable onDragStart={dragStart} className={classes} title={content}>
             {icon && <FontAwesomeIcon icon={icon} />}
             <div>{content}</div>
         </div>
